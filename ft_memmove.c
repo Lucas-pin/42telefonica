19744dc2270e8a1888705e95314355edb5d212ee
Lucas-pin <Lucasfacundopin@gmail.com>
2023-09-21 16:32:02 +0200

    Añadi la variable de distancia entre direcciones de puntero

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,21 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpin <lpin@student.42.malaga.com>          +#+  +:+       +#+        */
+/*   By: lpin < lpin@student.42malaga.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 13:18:47 by lpin              #+#    #+#             */
-/*   Updated: 2023/09/20 13:18:52 by lpin             ###   ########.fr       */
+/*   Updated: 2023/09/21 12:23:22 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	while (len > 0)
+	ptrdiff_t distance;
+
+	distance = src - dest;
+	if (distance > len)
 	{
-		*(char *)dest = *(char *)src;
-		--len;
+		while (len > 0)
+		{
+			*(char *)dest = *(char *)src;
+			--len;
+		}
+	}
+	else if (distance < len)
+	{
+		while (distance > 0)
+		{
+			*(char *)dest = *(char *)src;
+			--distance;
+		}
 	}
 }
