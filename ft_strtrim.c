3888e2a3d0ecce3d624a561872649200ba60ab31
Lucas Pin <lucasfacundopin@gmail.com>
2023-09-28 14:09:56 +0200

    finalice la funcion ft_strtrim.c, me falta añadirla al header

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,55 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpin < lpin@student.42malaga.com>          +#+  +:+       +#+        */
+/*   By: lpin <lpin@student.42.malaga.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:21:47 by lpin              #+#    #+#             */
-/*   Updated: 2023/09/27 13:17:12 by lpin             ###   ########.fr       */
+/*   Updated: 2023/09/28 14:08:27 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 
-char	*ft_reverse_strchr(const char *s, int c)
+size_t	ft_null_strchr(char *s, int c)
 {
-	int	i;
-	char	*temp;
+	size_t	i;
+	size_t	s_len;
+	size_t	null_counter;
 
 	i = 0;
-	temp = (char *)s;
-	while (temp[i] != (char)c && temp[i] != '\0')
+	s_len = ft_strlen(s);
+	null_counter = 0;
+	while (i < s_len)
+	{
+		if (s[i] == (char)c)
+		{
+			s[i] = '\0';
+			++null_counter;
+		}
 		++i;
-	temp[i] = '\0';
-	return (temp);
+	}
+	return (null_counter);
+}
+
+size_t	ft_memoffset(void const *s, int c, size_t len)
+{
+	size_t	offset;
+
+	offset = 0;
+	while (*(char *)s != c && len > 0)
+	{
+		++offset;
+		--len;
+		s++;
+	}
+	return (offset);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
 	char	*s1_trimmed;
-	char	*s1_concat;
 	size_t	s1_len;
+	size_t	offset;
+	size_t	null_counter;
 
-	s1_len = strlen(s1);
-	s1_trimmed = calloc((s1_len + 1),sizeof(char));
-	trim = ft_strchr(s1, set[0]);
-	trim++;
-	s1_concat = ft_reverse_strchr(s1, set[0]);
-	strlcat(s1_trimmed, s1_concat, s1_len);
-	s1_concat = ft_reverse_strchr(trim, set[0]);
-	trim = ft_strchr(trim, set[0]);
-	trim++;
-	strlcat(s1_trimmed, s1_concat, s1_len);
+	s1_len = ft_strlen(s1);
+	s1_trimmed = ft_calloc((s1_len + 1), sizeof(char));
+	if (s1_trimmed == NULL)
+		return (NULL);
+	s1_trimmed = (char *)s1;
+	while (*set)
+	{
+		null_counter = ft_null_strchr(s1_trimmed, *set);
+		offset = ft_memoffset(s1_trimmed, 0, s1_len);
+		while (null_counter > 0)
+		{
+			ft_memmove(s1_trimmed + offset, s1_trimmed
+				+ offset + 1, s1_len - offset);
+			offset = ft_memoffset(s1_trimmed, 0, s1_len);
+			--null_counter;
+		}
+		set++;
+	}
 	return (s1_trimmed);
 }
 
-int	main(void)
+/*int	main(void)
 {
-	char	s1[] = "Perritoperro";
-	char	set[] = "er";
+	char	s1[] = "tres tristes tigres comen trigo en un trigal";
+	char	set[] = "rti";
 	char	*ptr;
+	size_t	s1_len = strlen(s1);
+	size_t	i;
 
 	ptr = ft_strtrim(s1, set);
+	i = 0;
 	printf("El contenido de ptr es: %s\n", ptr);
 	return (0);
-}
+}*/
