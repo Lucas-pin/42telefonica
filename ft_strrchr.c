c07ff7aca781d49644288ddb765c1c55f6729a14
Lucas Pin <lucasfacundopin@gmail.com>
2023-09-28 18:32:42 +0200

    No tener en cuenta estas modificaciones

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,11 @@
 /*   By: lpin <lpin@student.42.malaga.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 21:45:32 by lpin              #+#    #+#             */
-/*   Updated: 2023/09/26 17:29:27 by lpin             ###   ########.fr       */
+/*   Updated: 2023/09/28 18:29:56 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "libft.h"
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -25,14 +25,13 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	return (temp);
 }
-/*
-int	main(void)
+
+/*int	main(void)
 {
-	char	arr[] = "Argentina";
+	char	arr[] = "Argentinaghfjhgjh";
 	char	c = 'n';
 
 	printf("El puntero contiene: %s\n", ft_strrchr(arr, c));
 	printf("El puntero contiene: %s\n", strrchr(arr, c));
 	return (0);
-}
-*/
+}*/
