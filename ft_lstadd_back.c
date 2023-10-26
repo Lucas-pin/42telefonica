2fb82a3c4de8c756cfd19ebf776ee56a14275589
Lucas-pin <Lucasfacundopin@gmail.com>
2023-10-26 17:03:14 +0200

    Agregue la comprobacion de errores a la función ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpin <lpin@student.42.malaga.com>          +#+  +:+       +#+        */
+/*   By: lpin < lpin@student.42malaga.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:43:14 by lpin              #+#    #+#             */
-/*   Updated: 2023/10/09 13:22:54 by lpin             ###   ########.fr       */
+/*   Updated: 2023/10/26 16:56:00 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,41 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*node;
 
-	node = *lst;
-	while ((*lst)->next)
-		*lst = (*lst)->next;
-	(*lst)->next = new;
-	*lst = node;
+	if (new == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		node = *lst;
+		*lst = ft_lstlast(*lst);
+		(*lst)->next = new;
+		*lst = node;
+	}
 }
 
 /*int	main(void)
 {
 	t_list	*lst;
 	t_list	*node_1;
-	t_list	*last_node;
 	char	a[] = "Hola";
 	char	b[] = "que";
 	char	c[] = "tal";
 	char	d[] = "?";
 	char	e[] = "Bien";
 
-	lst = ft_lstnew(d);
-	node_1 = ft_lstnew(c);
-	ft_lstadd_front(&lst, node_1);
+	lst = ft_lstnew(a);
 	node_1 = ft_lstnew(b);
-	ft_lstadd_front(&lst, node_1);
-	node_1 = ft_lstnew(a);
-	ft_lstadd_front(&lst, node_1);
+	ft_lstadd_back(&lst, node_1);
+	node_1 = ft_lstnew(c);
+	ft_lstadd_back(&lst, node_1);
+	node_1 = ft_lstnew(d);
+	ft_lstadd_back(&lst, node_1);
 	node_1 = ft_lstnew(e);
 	ft_lstadd_back(&lst, node_1);
-	printf("El contenido de la lista es: %s\n", lst->content);
-	last_node = ft_lstlast(lst);
-	printf("El content de last_node es: %s\n", (char *)last_node->content);
-	printf("El next de last_node es: %p\n", last_node->next);
 	while (lst)
 	{
-		printf("El contenido de la lista completa es: %s\n", lst->content);
+		printf("%s\n", (char *)lst->content);
 		lst = lst->next;
 	}
 	free(lst);
